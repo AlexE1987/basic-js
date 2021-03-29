01f713699ef49a5ed3e05855c0e259f331ff3280
AlexE1987 <evsikovad1987@gmail.com>
2021-03-29 12:29:04 +0400

    feat: solution task - transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(Array.isArray(arr) == false) throw new Error('Error');
+
+  const newArr = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+      if(arr[i] == '--discard-next') {
+          newArr[i] = 'empti';
+          newArr[i + 1] = 'empti';
+          i = i + 1;
+      } 
+      else if (arr[i] == '--discard-prev') {
+          newArr[i - 1] = 'empti';
+      }
+      else if (arr[i] == '--double-next') {
+          newArr[i] = arr[i + 1];
+      }
+      else if (arr[i] == '--double-prev') {
+          newArr[i] = newArr[i - 1];
+      } 
+      else {newArr[i] = arr[i]};             
+  };
+  
+  return newArr.filter((e) => e != '--discard-next' && e != '--discard-prev' && 
+                              e != '--double-next' && e != '--double-prev' && 
+                              e != 'empti' && e != undefined)
 };
