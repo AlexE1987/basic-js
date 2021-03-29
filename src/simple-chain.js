0c10c37f32a815d4140d506496d7b5b563ee1528
AlexE1987 <evsikovad1987@gmail.com>
2021-03-29 14:07:36 +0400

    feat: solution task - simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  chainArray: [],
+  getLength() { // возвращает текущую длину цепи в виде числа;
+    return this.chainArray.length
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) { // добавляет звено, содержащее строковое представление value к цепочке
+    const chain = `( ${value} )`;
+    this.chainArray.push(chain);
+    return this;
   },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) { //удаляет звено цепи, находящееся в заданном положении;
+    if(position < 1 || position > this.chainArray.length || Number.isInteger(position) == false) {
+      this.chainArray = []; throw new Error('Error');
+    }
+    this.chainArray.splice(position - 1, 1);    
+    return this;
   },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  reverseChain() { // разворачивает цепь задом наперед;
+    this.chainArray.reverse();
+    return this;
   },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  finishChain() { // завершает цепь и возвращает ее.
+    const finish = this.chainArray.join('~~');
+    this.chainArray = [];
+    return finish;
   }
 };
 
